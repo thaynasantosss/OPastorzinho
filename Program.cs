126638d5c95d590b6b08171aa3ac7182d45a58a0
thaynasantosss <thayna.2015.09@gmail.com>
2023-03-03 22:47:55 -0300

    Concluí  primeira parte

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Dó, ré, mi, fá, fá, fá,");
+Console.WriteLine("Dó,ré, dó, ré, ré, ré,");
+Console.WriteLine("Dó, sol, fá, mi, mi, mi,");
+Console.WriteLine("Dó, ré, mi, fá, fá, fá.");
